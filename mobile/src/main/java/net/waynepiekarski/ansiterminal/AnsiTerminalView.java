a572031457763feadf42808ea56771d24241c9f0
Wayne Piekarski <wayne@tinmith.net>
2014-07-11 18:29:51 -0700

    Implement automatic selection of the font size based on the size of the display

--- mobile/src/main/java/net/waynepiekarski/ansiterminal/AnsiTerminalView.java
+++ mobile/src/main/java/net/waynepiekarski/ansiterminal/AnsiTerminalView.java
@@ -21,9 +21,14 @@ public class AnsiTerminalView extends SurfaceView implements SurfaceHolder.Callb
         public Paint mPaintText;
         public int mCanvasWidth;
         public int mCanvasHeight;
+        public boolean mCanvasDirty;
         public int mCharWidth;
         public int mCharHeight;
+        public int mCharWidthOffset;
+        public int mCharHeightOffset;
         public static final int mCharSpacing = 1;
+        public static final int mTerminalWidth = 80;
+        public static final int mTerminalHeight = 25;
 
         public RenderThread (SurfaceHolder holder, Context context) {
             mSurfaceHolder = holder;
@@ -31,21 +36,52 @@ public class AnsiTerminalView extends SurfaceView implements SurfaceHolder.Callb
 
             mPaintText = new Paint();
             mPaintText.setTypeface(Typeface.create("Monospace", Typeface.BOLD));
-            mPaintText.setTextSize(50);
             mPaintText.setARGB(0xFF, 0x0, 0x0, 0xFF);
-            Rect rect = new Rect();
-            mPaintText.getTextBounds("X", 0, 1, rect);
-            mCharWidth = rect.width() + mCharSpacing;
-            mCharHeight = rect.height() + mCharSpacing;
-            Logging.debug("Calculated bounds width=" + mCharWidth + " height=" + mCharHeight);
 
             mPaintBounds = new Paint();
             mPaintBounds.setARGB(0xFF, 0x0, 0xFF, 0x00);
+
+            mCanvasDirty = true;
+        }
+
+        // Given the current canvas dimensions, find the font size which fills up the screen
+        // as much as possible given the desired terminal dimensions
+        public void recalculateFontSize (Canvas canvas, int terminalWidth, int terminalHeight) {
+            int fontSize = 1;
+            int lastWidth = -1;
+            int lastHeight = -1;
+            while (true) {
+                if (fontSize >= 1000)
+                    Logging.fatal("Unable to find suitable font size to fill canvas");
+                mPaintText.setTextSize(fontSize);
+                Rect rect = new Rect();
+                mPaintText.getTextBounds("X", 0, 1, rect); // How big is "X" when drawn
+                if ((rect.width() == 0) || (rect.height() == 0))
+                    Logging.fatal("Invalid width or height from getTextBounds");
+                lastWidth = mCharWidth;
+                lastHeight = mCharHeight;
+                mCharWidth = rect.width() + mCharSpacing;
+                mCharHeight = rect.height() + mCharSpacing;
+                Logging.debug("Calculated for fontsize=" + fontSize + " bounds width=" + mCharWidth + " height=" + mCharHeight);
+                if ((mCharWidth * terminalWidth > mCanvasWidth) || (mCharHeight * terminalHeight > mCanvasHeight)) {
+                    // This font size is too big, we need to go back one step and finish
+                    fontSize--;
+                    mCharWidth = lastWidth;
+                    mCharHeight = lastHeight;
+                    if ((mCharWidth <= 0) || (mCharHeight <= 0))
+                        Logging.fatal("Failed to compute expected width and height");
+                    // Compute any remaining padding to center the image on the display
+                    mCharWidthOffset = (mCanvasWidth - mCharWidth*terminalWidth) / 2;
+                    mCharHeightOffset = (mCanvasHeight - mCharHeight*terminalHeight) / 2;
+                    return;
+                }
+                fontSize++;
+            }
         }
 
         public void drawFixedChar (Canvas canvas, char ch, int row, int col) {
-            int x = col * mCharWidth;
-            int y = (row+1) * mCharHeight;
+            int x = mCharWidthOffset + col * mCharWidth;
+            int y = mCharHeightOffset + (row+1) * mCharHeight;
             canvas.drawText(String.valueOf(ch), x, y, mPaintText);
         }
 
@@ -72,6 +108,11 @@ public class AnsiTerminalView extends SurfaceView implements SurfaceHolder.Callb
         int tempR = 0;
         int tempC = 0;
         public void doDraw(Canvas canvas) {
+            if (mCanvasDirty) {
+                mCanvasDirty = false;
+                recalculateFontSize(canvas, mTerminalWidth, mTerminalHeight);
+            }
+
             // Logging.debug("onDraw temp=" + tempX);
             canvas.drawRGB(0xC0, 0xC0, 0xC0); // Gray terminal background
 
