a6ff259239ee1985dc16f615f6a3d9e32b7dac14
Wayne Piekarski <wayne@tinmith.net>
2014-07-16 16:12:43 -0700

    Port Glider over to the Google TV template

--- tv/src/main/java/net/waynepiekarski/ansiterminal/MyActivity.java
+++ tv/src/main/java/net/waynepiekarski/ansiterminal/MyActivity.java
@@ -8,6 +8,7 @@ public class MyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        AnsiTerminalView ansi = new AnsiTerminalView (this);
+        setContentView(ansi);
     }
 }
