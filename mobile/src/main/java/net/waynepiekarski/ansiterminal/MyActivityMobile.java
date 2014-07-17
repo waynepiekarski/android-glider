c15c1a88a69902cb2d1ae99b06e98a2bc6f65c46
Wayne Piekarski <wayne@tinmith.net>
2014-07-16 17:06:35 -0700

    Convert the phone version of the game to use a full-screen and not show the action bar

--- mobile/src/main/java/net/waynepiekarski/ansiterminal/MyActivityMobile.java
+++ mobile/src/main/java/net/waynepiekarski/ansiterminal/MyActivityMobile.java
@@ -1,43 +1,15 @@
 package net.waynepiekarski.ansiterminal;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 
-public class MyActivityMobile extends ActionBarActivity {
+public class MyActivityMobile extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
 
-        // Grab the layout object to embed later things in code
-        RelativeLayout rel = (RelativeLayout)findViewById(R.id.top_layout);
-
-        AnsiTerminalView ansi = new AnsiTerminalView(this);
-        rel.addView(ansi);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        setContentView(new AnsiTerminalView(this));
     }
 }
