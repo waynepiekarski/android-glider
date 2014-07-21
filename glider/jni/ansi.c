87326561a5b1c9199443ddc763f69d108889fce6
Wayne Piekarski <wayne@tinmith.net>
2014-07-21 16:53:53 -0700

    Apply GPLv3 license to all Java and C files using update-license.sh

--- glider/jni/ansi.c
+++ glider/jni/ansi.c
@@ -1,10 +1,27 @@
-/*
-  ansi.c
-  Copyright (C) 1996 - The Dark Scythe Corporation
-  Written by Wayne Piekarski
+// ---------------------------------------------------------------------
+//
+// Glider
+//
+// Copyright (C) 1996-2014 Wayne Piekarski
+// wayne@tinmith.net http://tinmith.net/wayne
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ---------------------------------------------------------------------
 
-  Enable DEBUG_ANSI to show debugging messages
-*/
+
+// Enable DEBUG_ANSI to show debugging messages
 
 #include <stdio.h>
 #include "ansi.h"
